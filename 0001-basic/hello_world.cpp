3f0e073fe798c17abfd81eb73b921e0bbfa82302
EsteMaeLeo <leonel_morales20@hotmail.com>
2026-05-20 23:53:16 -0500

    first steps

---
+++ 0001-basic/hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+//Main function
+int main(){
+//output text
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
---
+++ 0001-basic/personal_info.cpp
@@ -0,0 +1,58 @@
+/*
+* Personal Introduction Program
+* Created by: Leonel Morales
+* Date: 05-19-2026
+*
+* Description: This program displays personal information
+* about myself in a formated way on the console.
+*/
+/*
+* My Learning Goals:
+*
+* Efficiently manage data using appropriate data types, variables, and operators.
+* Build sophisticated program logic with control flow mechanisms.
+* Diagnose and fix syntax and logical errors to improve reliability.
+*/
+//include necessary header
+#include <iostream>
+
+// main function program execution starts her
+int main(){
+    //display header
+    std::cout << "====================================" << std::endl;
+    std::cout << "       PERSONAL INTRODUCTION        " << std::endl;
+    std::cout << "====================================" << std::endl;
+
+    // Display basic info
+    std::cout << "Hello, my name is Leonel Morales." << std::endl;
+    std::cout << "Software Developer/ ABAP senior" << std::endl;
+    std::cout << "Hobbies: Read, Gym, Video Games" << std::endl;
+    // Educational background section 
+    std::cout << "\nEDUCATION" << std::endl;
+    std::cout << "------------------------------------" << std::endl;
+    std::cout << "Education: System Engineering" << std::endl;
+    std::cout << "School: UTECH" << std::endl;
+    std::cout << "Year: 2007" << std::endl;
+    // Career goals
+    std::cout << "\nCAREER GOALS" << std::endl;
+    std::cout << "------------------------------------" << std::endl;
+    std::cout << "Short-term: Get new position" << std::endl;
+    std::cout << "Long-term: Transition to AI or Cybersecurity" << std::endl;
+    // Why taking this course
+    std::cout << "\nWHY I'M TAKING THIS COURSE" << std::endl;
+    std::cout << "------------------------------------" << std::endl;
+    std::cout << "Get new skills with C++" << std::endl;
+    //  Favorite programming concepts
+    std::cout << "\nTOPICS I'M EXCITED ABOUT" << std::endl;
+    std::cout << "------------------------------------" << std::endl;
+    std::cout << "Data Types" << std::endl;
+    std::cout << "------------------------------------" << std::endl;
+    std::cout << "\33[34mThank you for reading my introduction!\33[340" << std::endl;
+    // Display footer
+    std::cout << "\33[31m====================================" << std::endl;
+    // Display footer
+    std::cout << "====================================" << std::endl;
+
+    // Return statement
+    return 0;
+}
